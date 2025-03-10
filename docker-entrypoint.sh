7dbb371b4f4d0ff908044dbf76440a915570dbbb
mu_ibragimov <m.ibragimov2@uzinfocom.uz>
2025-03-10 18:08:01 +0500

    add .env

---
+++ docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+flask db upgrade
+exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
--- models/user.py
+++ models/user.py
@@ -7,4 +7,4 @@ class UserModel(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80),  unique=True,  nullable = False)
-    password = db.Column(db.String(80),  nullable = False)
+    password = db.Column(db.String(256),  nullable = False)
